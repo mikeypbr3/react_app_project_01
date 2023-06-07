20942f47d2e474fa6d81796433c4e8ce902556b7
mikeypbr3 <130999289+mikeypbr3@users.noreply.github.com>
2023-06-07 12:13:32 -0400

    updated app.js/index.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React from 'react';
-import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Upload from './components/Upload';
 import Filter from './components/Filter';
 
 function App() {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/upload">Upload</Link>
-            </li>
-            <li>
-              <Link to="/filter">Filter</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <Switch>
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/upload" component={Upload} />
-          <Route path="/filter" component={Filter} />
-          <Redirect to="/dashboard" />
-        </Switch>
-      </main>
+    <div className="app">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/upload">Upload</Link>
+          </li>
+          <li>
+            <Link to="/filter">Filter</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route exact path="/upload">
+          <Upload />
+        </Route>
+        <Route exact path="/filter">
+          <Filter />
+        </Route>
+      </Switch>
     </div>
   );
 }
